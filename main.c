9e614dedd2659db945ae78f9af2c6febacd6241c
vpc20 <cuevasvp@gmail.com>
2025-11-30 13:40:35 +0800

    revisions

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 
 #include "aoc_tools.h"
 
-char **str_split(char *str, char delim) {
+// the delimiter can be any one of the char in delim or the whole string itself
+char **str_split(const char *str, const char *delim) {
     // Make a copy since strtok modifies the string
     char *str_copy = malloc(strlen(str) + 1);
     if (!str_copy) return NULL;
@@ -12,11 +13,10 @@ char **str_split(char *str, char delim) {
 
     // Count tokens first
     int count = 0;
-    char delim_str[2] = {delim, '\0'};
-    char *token = strtok(str_copy, delim_str);
+    char *token = strtok(str_copy, delim);
     while (token != NULL) {
         count++;
-        token = strtok(NULL, delim_str);
+        token = strtok(NULL, delim);
     }
 
     // Allocate array
@@ -31,12 +31,12 @@ char **str_split(char *str, char delim) {
 
     // Split and store tokens
     int idx = 0;
-    token = strtok(str_copy, delim_str);
+    token = strtok(str_copy, delim);
     while (token != NULL) {
         str_arr[idx] = malloc(strlen(token) + 1);
         strcpy(str_arr[idx], token);
         idx++;
-        token = strtok(NULL, delim_str);
+        token = strtok(NULL, delim);
     }
 
     str_arr[idx] = NULL;
@@ -47,67 +47,102 @@ char **str_split(char *str, char delim) {
 int main(int argc, char *argv[]) {
     printf("Hello, World!\n");
 
-    char **grid = input_to_grid("/home/vpc/CLionProjects/c-stuff/input.txt");
+    //-------------------------------------------------------------------------
+    // char **grid = input_to_grid("/home/vpc/CLionProjects/c-stuff/input.txt");
+    //
+    // // Print the 2D array
+    // // printf("File contents (%d lines):\n", num_lines);
+    // printf("------------------------\n");
+    // for (int i = 0; grid[i] != NULL; i++) {
+    //     printf("Line %d: %s\n", i, grid[i]);
+    // }
+    //
+    // // Access individual characters
+    // printf("\n--- Accessing as 2D array ---\n");
+    // for (int i = 0; grid[i] != NULL; i++) {
+    //     printf("Line %d characters: ", i);
+    //     for (int j = 0; grid[i][j] != '\0'; j++) {
+    //         printf("%c ", grid[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // free(grid)
 
-    // Print the 2D array
-    // printf("File contents (%d lines):\n", num_lines);
-    printf("------------------------\n");
-    for (int i = 0; grid[i] != NULL; i++) {
-        printf("Line %d: %s\n", i, grid[i]);
+
+    //-------------------------------------------------------------------------
+    // Single character delimiter
+    char **parts = str_split("apple,banana,cherry", ",");
+    for (int i = 0; parts[i] != NULL; i++) {
+        printf("%s\n", parts[i]);
     }
+    free(parts);
 
-    // Access individual characters
-    printf("\n--- Accessing as 2D array ---\n");
-    for (int i = 0; grid[i] != NULL; i++) {
-        printf("Line %d characters: ", i);
-        for (int j = 0; grid[i][j] != '\0'; j++) {
-            printf("%c ", grid[i][j]);
-        }
-        printf("\n");
+    // Multi-character delimiter
+    parts = str_split("apple::banana::cherry", "::");
+    for (int i = 0; parts[i] != NULL; i++) {
+        printf("%s\n", parts[i]);
     }
+    free(parts);
 
-    // char *test = "qqq,www,eee,rrr";
-    char **str_arr = str_split("qqq,www,eee,rrr", ',');
-    for (int i = 0; str_arr[i] != NULL; i++) {
-        printf("%d: %s\n", i, str_arr[i]);
+    // Multiple delimiter characters (splits on ANY of them)
+    parts = str_split("apple,banana;cherry", ",;");
+    for (int i = 0; parts[i] != NULL; i++) {
+        printf("%s\n", parts[i]);
     }
+    free(parts);
 
-    char *substr;
-    substring(substr, "qwerasdf", 0, 4);
-    printf("%s\n", substr);
+    // Whitespace delimiters
+    parts = str_split("apple banana\tcherry", " \t");
+    for (int i = 0; parts[i] != NULL; i++) {
+        printf("%s\n", parts[i]);
+    }
+    free(parts);
 
-    substring(substr, "qwerasdf", 1, 4);
-    printf("%s\n", substr);
+    printf("last one\n");
+    // Whitespace delimiters
+    parts = str_split("apple,banana;cherry,;grapes", ",;");
+    for (int i = 0; parts[i] != NULL; i++) {
+        printf("%s\n", parts[i]);
+    }
+    free(parts);
 
-    substring(substr, "qwerasdf", 2, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 3, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 4, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 5, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 6, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 7, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 8, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 0, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 8, 4);
-    printf("%s\n", substr);
-
-    substring(substr, "qwerasdf", 0, 4);
-    printf("%s\n", substr);
+    //-------------------------------------------------------------------------
+    // char *substr;
+    // substring(substr, "qwerasdf", 0, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 1, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 2, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 3, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 4, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 5, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 6, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 7, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 8, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 0, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 8, 4);
+    // printf("%s\n", substr);
+    //
+    // substring(substr, "qwerasdf", 0, 4);
+    // printf("%s\n", substr);
 
     return 0;
 }
